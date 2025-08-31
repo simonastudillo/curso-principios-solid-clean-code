1155903df5da65a0ef98c121d8035e72992b76c9
Simon Astudillo <spastudilloq@gmail.com>
2025-08-31 19:02:19 -0300

    17.Ejercicio con funciones

--- cleanSolid/src/clean-code/03-funciones.ts
+++ cleanSolid/src/clean-code/03-funciones.ts
@@ -1,34 +1,45 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
+    function getMovieById( id: string ): void {
+        console.log({ id });
     }
 
     // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    function getMovieCastById( id: string ): void {
         console.log({ id });
     }
 
     // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    function getActorBiographyById( id: string ): void {
+        console.log({ id });
     }
     
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    interface Movie {
+      cast:          string[];
+      description:   string;
+      rating:        number;
+      title:         string;
+    }
+    function createMovie( { title, description, rating, cast }: Movie ): void {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
+    function checkFullName( fullName: string): boolean {
+         // tarea asincrona para verificar nombre
         // ..
         // ..
-        if ( fullName === 'fernando' ) return false;
+        // Simulando una verificación de nombre
+        return fullName !== 'fernando';
+    }
+    function createActor( fullName: string, birthDate: Date ): boolean {
 
-        console.log('Crear actor');
+
+        if ( !checkFullName(fullName) ) return false;
+
+        console.log('Crear actor', birthDate);
         return true;        
 
     }
