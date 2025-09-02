e26d31ca59fcebc0d0ca7c07ba41ca59d1c5ac42
Simon Astudillo <spastudilloq@gmail.com>
2025-09-01 22:36:27 -0300

    27.Herencia - Problemática

--- cleanSolid/src/clean-code/06-classes-a.ts
+++ cleanSolid/src/clean-code/06-classes-a.ts
@@ -1,5 +1,7 @@
 (() => {
 
+   // No aplicando el principio de responsabilidad única
+
    type Gender = 'M' | 'F';
 
    class Person {
@@ -11,7 +13,49 @@
       ) {}
    }
 
-   const newPerson = new Person('Fernando', 'M', new Date('1985-10-21'));
-   console.log({newPerson});
+   class User extends Person {
+      private lastAccess: Date;
+
+      constructor(
+         public email: string,
+         public role: string,
+         name: string,
+         gender: Gender,
+         birthdate: Date
+      ) {
+         super(name, gender, birthdate);
+         this.lastAccess = new Date();
+      }
+
+      checkCredentials() {
+         // Logic to check user credentials
+         return true;
+      }
+   }
+
+   class UserSettings extends User {
+      constructor(
+         public workingDirectory: string,
+         public lastOpenFolder: string,
+         email: string,
+         role: string,
+         name: string,
+         gender: Gender,
+         birthdate: Date
+      ) {
+         super(email, role, name, gender, birthdate);
+      }
+   }
+
+   const userSettings = new UserSettings(
+      'C:/projects/clean-code',
+      'C:/projects/clean-code/src',
+      'fernando@google.com',
+      'admin',
+      'Fernando',
+      'M',
+      new Date('1985-10-21')
+   );
+   console.log({userSettings});
 
 })();
