ab602ed1c1f12404eff313f0920a6a5a5683c9f5
Simon Astudillo <spastudilloq@gmail.com>
2025-09-05 22:02:46 -0300

    52.Ejercicio de OCP

---
+++ solid/02-open-close-c.ts
@@ -0,0 +1,10 @@
+import axios from '../cleanSolid/node_modules/axios/index.js';
+
+export class HttpClient {
+
+   async get(url: string) {
+      const { data, status } = await axios.get(url);
+      console.log(status);
+      return { data, status };
+   }
+}
