2ed190fdcdaac8cff47f3390e27e5a702ba7db30
Simon Astudillo <spastudilloq@gmail.com>
2025-09-05 23:15:24 -0300

    60.Aplicar el principio de segregación de interfaz

--- solid/04-segregation.ts
+++ solid/04-segregation.ts
@@ -1,66 +1,58 @@
 (() => {
 
-interface Bird {
-   fly(): number;
-   eat(): void;
-   run(): void;
-   swim(): void;
-}
-
-class Tucan implements Bird {
-   public fly(): void {
-      console.log('El tucán vuela');
-   }
-   public eat(): void {
-      console.log('El tucán come frutas');
-   }
-   public run(): void {
-      console.log('El tucán corre');
-   }
-   public swim(): void {}
-}
-
-class Humminbird implements Bird {
-   public fly(): void {
-      console.log('El colibrí vuela');
-   }
-   public eat(): void {
-      console.log('El colibrí come néctar');
-   }
-   public run(): void {
-      console.log('El colibrí corre');
-   }
-   public swim(): void { }
-}
-
-class Ostrich implements Bird {
-   public fly(): void {
-      throw new Error('El avestruz no vuela');
-   }
-   public eat(): void {
-      console.log('El avestruz come semillas');
-   }
-   public run(): void {
-      console.log('El avestruz corre');
-   }
-   public swim(): void { }
-}
-
-class Penguin implements Bird {
-   public fly(): void {
-      throw new Error('El pingüino no vuela');
-   }
-   public eat(): void {
-      console.log('El pingüino come peces');
-   }
-   public run(): void {
-      console.log('El avestruz corre');
+   interface Bird {
+      eat(): void;
    }
 
-   public swim(): void {
-      console.log('El pingüino nada en el agua');
+   interface Flyer {
+      fly(): void;
    }
-}
+
+   interface Swimmer {
+      swim(): void;
+   }
+
+   interface Runner {
+      run(): void;
+   }
+
+   class Tucan implements Bird, Flyer {
+      public fly(): void {
+         console.log('El tucán vuela');
+      }
+      public eat(): void {
+         console.log('El tucán come frutas');
+      }
+   }
+
+   class Humminbird implements Bird, Flyer {
+      public fly(): void {
+         console.log('El colibrí vuela');
+      }
+      public eat(): void {
+         console.log('El colibrí come néctar');
+      }
+   }
+
+   class Ostrich implements Bird, Swimmer, Runner {
+      public eat(): void {
+         console.log('El avestruz come semillas');
+      }
+      public run(): void {
+         console.log('El avestruz corre');
+      }
+      public swim(): void { }
+   }
+
+   class Penguin implements Bird, Swimmer {
+      public eat(): void {
+         console.log('El pingüino come peces');
+      }
+
+      public swim(): void {
+         console.log('El pingüino nada en el agua');
+      }
+   }
 
 
 })();
