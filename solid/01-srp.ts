21d9b6115cabbf09b493b9f1c79dee944713a439
Simon Astudillo <spastudilloq@gmail.com>
2025-09-05 21:28:15 -0300

    49.Ejemplo de SRP - Segunda parte

--- solid/01-srp.ts
+++ solid/01-srp.ts
@@ -20,22 +20,37 @@
       }
    }
 
+   class Mailer {
+      private emailService: Object;
+
+      sendEmail(email: string, subject: string, body: string) {
+         // Lógica para enviar un correo electrónico
+         console.log('Enviando correo a:', email);
+      }
+   }
+
    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
    class ProductBloc {
 
+      private productService: ProductService;
+      private mailer: Mailer;
+
+      constructor(productService: ProductService, mailer: Mailer) {
+         this.productService = productService;
+         this.mailer = mailer;
+      }
+
       loadProduct(id: number) {
-         // Realiza un proceso para obtener el producto y retornarlo
-         console.log('Producto: ', { id, name: 'OLED Tv' });
+         this.productService.getProduct(id);
       }
 
       saveProduct(product: Product) {
-         // Realiza una petición para salvar en base de datos 
-         console.log('Guardando en base de datos', product);
+         this.productService.saveProduct(product);
       }
 
       notifyClients() {
-         console.log('Enviando correo a los clientes');
+         this.mailer.sendEmail('cliente@example.com', 'Nuevo producto', 'Se ha agregado un nuevo producto');
       }
 
    }
@@ -53,7 +68,7 @@
 
 
 
-   const productBloc = new ProductBloc();
+   const productBloc = new ProductBloc(new ProductService(), new Mailer());
    const cartBloc = new CartBloc();
 
    productBloc.loadProduct(10);
