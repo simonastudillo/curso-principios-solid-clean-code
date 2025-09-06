4e374411bfd611766d11e227f8987b312cc2c17d
Simon Astudillo <spastudilloq@gmail.com>
2025-09-05 21:14:07 -0300

    48.Ejemplo de SRP

--- solid/01-srp.ts
+++ solid/01-srp.ts
@@ -5,6 +5,21 @@
       name: string;
    }
 
+   class ProductService {
+
+      private httpAdapter: Object;
+
+      getProduct(id: number): void {
+         // Realiza un proceso para obtener el producto y retornarlo
+         console.log('Producto: ', { id, name: 'OLED Tv' });
+      }
+
+      saveProduct(product: Product) {
+         // Realiza una petición para salvar en base de datos 
+         console.log('Guardando en base de datos', product);
+      }
+   }
+
    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
    class ProductBloc {
@@ -23,21 +38,28 @@
          console.log('Enviando correo a los clientes');
       }
 
+   }
+
+   class CartBloc {
+
+      private $itemsInCart: Object[] = [];
+
       onAddToCart(productId: number) {
          // Agregar al carrito de compras
+         this.$itemsInCart.push({ id: productId });
          console.log('Agregando al carrito ', productId);
       }
-
    }
 
 
 
    const productBloc = new ProductBloc();
+   const cartBloc = new CartBloc();
 
    productBloc.loadProduct(10);
    productBloc.saveProduct({ id: 10, name: 'OLED TV' });
    productBloc.notifyClients();
-   productBloc.onAddToCart(10);
+   cartBloc.onAddToCart(10);
 
 
 
